7cf2a95f37927dcd2584617c2a30b7f60cffd9d6
mlukacs <mlukacs@deezer.com>
2023-01-16 17:35:31 +0100

    evolution of minimun ios for package

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SimpleNetwork",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v15),
         .tvOS(.v16),
         .watchOS(.v8),
         .macOS(.v13)
